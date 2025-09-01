72c0cbacef13c652feff6b73878829191d9ae224
TimmyLEEY <afolabitimilehin12@gmail.com>
2025-09-01 14:42:35 -0700

    cors issueee

--- server.js
+++ server.js
@@ -11,10 +11,23 @@ import ebookRoutes from "./routes/ebookRoutes.js";
 dotenv.config();
 connectDB();
 
+const allowedOrigins = [
+  'http://localhost:3000',                       // local frontend
+  'https://konasalinsurance.netlify.app'        // deployed frontend
+];
+
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:3000","https://konasalinsurance.netlify.app/"],
-  credentials: true, // if you use cookies/auth headers
+  origin: function(origin, callback){
+    // allow requests with no origin (like Postman)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true  // if using cookies or auth headers
 }));
 app.use(express.json());
 
